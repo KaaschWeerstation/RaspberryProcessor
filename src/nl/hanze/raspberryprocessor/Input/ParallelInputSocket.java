65f78505e2998ca12273e9428069104df5c59d70
Prusias <mikejordy1999@gmail.com>
2020-02-07 09:11:25 +0100

    Working on new Input model
    Signed-off-by: Prusias <mikejordy1999@gmail.com>

--- src/nl/hanze/raspberryprocessor/Input/ParallelInputSocket.java
+++ src/nl/hanze/raspberryprocessor/Input/ParallelInputSocket.java
@@ -3,8 +3,8 @@ package nl.hanze.raspberryprocessor.Input;
 import nl.hanze.raspberryprocessor.Data.Measurement;
 import nl.hanze.raspberryprocessor.Data.MeasurementInputQueue;
 import nl.hanze.raspberryprocessor.Main;
-import nl.hanze.raspberryprocessor.Utility.SemaphoreInteger;
 import nl.hanze.raspberryprocessor.Utility.DecimalInt;
+import nl.hanze.raspberryprocessor.Utility.SemaphoreInteger;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
@@ -15,270 +15,97 @@ import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.events.EndElement;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
-import javax.xml.transform.stax.StAXSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStreamReader;
-import java.io.StringReader;
+import java.io.*;
 import java.net.Socket;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-import static java.time.temporal.ChronoField.SECOND_OF_DAY;
-
 public class ParallelInputSocket implements Runnable {
-
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-ddHH:mm:ss");
-    private static final XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
-    private static final SchemaFactory  schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-
-    private MeasurementInputQueue measurementInputQueue;
-    //private XMLInputFactory xmlInputFactory;
-    private XMLEventReader xmlEventReader;
-    private BufferedReader bufferedReader;
-    //private SchemaFactory schemaFactory;
-    private Schema schema;
-    private Validator validator;
-
-    private StringReader stringReader;
-    private String readString;
-    private StringBuilder stringBuilder;
 
     private Socket connection;
 
-    private Measurement measurement;
-    private String measurementDateString;
-    private String currentXMLElement;
+    private MeasurementInputQueue measurementInputQueue;
 
-    private SemaphoreInteger semaphoreInteger;
+    private BufferedInputStream inputStream;
 
-    ParallelInputSocket(MeasurementInputQueue measurementInputQueue, Socket connection, SemaphoreInteger semaphoreInteger) {
+    ParallelInputSocket(MeasurementInputQueue measurementInputQueue, Socket connection, SemaphoreInteger semaphoreInteger) throws IOException {
         this.measurementInputQueue = measurementInputQueue;
-        //xmlInputFactory = XMLInputFactory.newInstance();
         this.connection = connection;
-
-        //schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-        try {
-            bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            //xmlEventReader = xmlInputFactory.createXMLEventReader(bufferedReader);
-
-            schema = schemaFactory.newSchema(new File("src/nl/hanze/raspberryprocessor/Resources/example.xsd"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        validator = schema.newValidator();
-        validator.setErrorHandler(new EmptyErrorHandler());
-
-        stringBuilder = new StringBuilder();
-
-        this.semaphoreInteger = semaphoreInteger;
-
-//        xmlEventReader = new EventReaderDelegate(xmlEventReader) {
-//            public XMLEvent nextEvent() throws XMLStreamException {
-//                XMLEvent event = super.nextEvent();
-//
-//                handleEvent(event);
-//
-//                return event;
-//            }
-//        };
+        this.inputStream = new BufferedInputStream(connection.getInputStream());
     }
 
-    // Split each "Burst" of data on <MEASUREMENT>
-    //
-    // Then pump it into a StringReader
-    // Validate that
-    // If valid then actually process it.
+    private static byte[] bytes_MEA = {77, 69, 65};
+    private static byte[] bytes_STN = {83, 84, 78};
+    private static byte[] bytes_TIM = {84, 73, 77};
+    private static byte[] bytes_TEM = {84, 69, 77};
+    private static byte[] bytes_DEW = {68, 69, 87};
+    private static byte[] bytes_STP = {83, 84, 80};
+    private static byte[] bytes_SLP = {83, 76, 80};
+    private static byte[] bytes_VIS = {86, 73, 83};
+    private static byte[] bytes_WDS = {87, 68, 83};
+    private static byte[] bytes_PRC = {80, 82, 67};
+    private static byte[] bytes_SND = {83, 78, 68};
+    private static byte[] bytes_FRS = {70, 82, 83};
+    private static byte[] bytes_CLD = {67, 76, 68};
+    private static byte[] bytes_WND = {87, 78, 68};
 
     @Override
     public void run() {
-        while(!connection.isClosed()) {
+        while(true) {
             try {
-                // Read the Socket bufferedReader (Input)
-                readString = bufferedReader.readLine();
+                // Assuming we are only receiving ascii
+                byte aByte = (byte) inputStream.read();
 
-                if (readString != null) {
-                    readString = readString.trim(); // Trim unwanted spaces/tabs
+                if (aByte == 60) {
+                    byte[] bytes = inputStream.readNBytes(3);
 
-                    // Are we at the start of a XML data blob?
-                    if (readString.equals("<MEASUREMENT>")) {
-                        stringBuilder = new StringBuilder();
-                        stringBuilder.append(readString);
+                    // The first measurement we expect
+                    if (bytes == bytes_STN) {
+                        inputStream.skipNBytes(1);
 
-                    // Are we at the end of a XML data blob?
-                    } else if (readString.equals("</MEASUREMENT>")) {
-                        stringBuilder.append(readString);
 
-                        String result = stringBuilder.toString();
-                        stringReader = new StringReader(result);
-                        xmlEventReader = xmlInputFactory.createXMLEventReader(stringReader);
 
-                        while (xmlEventReader.hasNext()) {
-                            handleEvent(xmlEventReader.nextEvent());
-                        }
-//                        String result = stringBuilder.toString(); // Turn the result into a string
-//                        if (isXMLValid(result)) {
-//                            try {
-//                                stringReader = new StringReader(result);
-//                                xmlEventReader = xmlInputFactory.createXMLEventReader(stringReader);
-//
-//                                while (xmlEventReader.hasNext()) {
-//                                    handleEvent(xmlEventReader.nextEvent());
-//                                }
-//
-//                            } catch (Exception e) {
-//                                e.printStackTrace();
-//                            }
-//                        }
-                    // Somewhere in the middle, just append.
-                    } else {
-                        stringBuilder.append(readString);
+                    } else if (bytes == bytes_TIM) {
+
+                    } else if (bytes == bytes_TEM) {
+
+                    } else if (bytes == bytes_DEW) {
+
+                    } else if (bytes == bytes_STP) {
+
+                    } else if (bytes == bytes_SLP) {
+
+                    } else if (bytes == bytes_VIS) {
+
+                    } else if (bytes == bytes_WDS) {
+
+                    } else if (bytes == bytes_PRC) {
+
+                    } else if (bytes == bytes_SND) {
+
+                    } else if (bytes == bytes_FRS) {
+
+                    } else if (bytes == bytes_CLD) {
+
+                    } else if (bytes == bytes_WND) {
+
                     }
-                } else {
-                    connection.close();
                 }
-            } catch (Exception e) {
+
+
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
 
-        // Connection is closed
-        System.out.println("Connection is closed");
-        try {
-            semaphoreInteger.decrement();
-        } catch (Exception e) {
-            e.printStackTrace();
+
         }
     }
 
-    private boolean isXMLValid(String xml) {
+    private int readInt() {
 
-        stringReader = new StringReader(xml);
-        try {
-            xmlEventReader = xmlInputFactory.createXMLEventReader(stringReader);
-            //validator.validate((new StAXSource(xmlEventReader)));
-        } catch (Exception e) {
-            //System.out.println("INVALID XML");
-            return false;
-            //e.printStackTrace();
-        }
-
-        //System.out.println("VALID XML");
-        return true;
-    }
-
-    private boolean trackElement;
-    private void handleEvent(XMLEvent nextEvent) {
-        if (nextEvent.isStartElement()) {
-            StartElement startElement = nextEvent.asStartElement();
-            currentXMLElement = startElement.getName().getLocalPart();
-
-            if(currentXMLElement.equals("MEASUREMENT")) {
-                trackElement = true;
-                measurement = new Measurement();
-            }
-        } else if (nextEvent.isEndElement()) {
-            EndElement endElement = nextEvent.asEndElement();
-            currentXMLElement = endElement.getName().getLocalPart();
-
-            if(currentXMLElement.equals("MEASUREMENT")) {
-                if (trackElement) {
-                    measurementInputQueue.add(measurement);
-                   // measurement.verifyValues();
-                }
-            }
-
-        } else {
-            if(currentXMLElement != null && !nextEvent.toString().trim().equals("") && trackElement) {
-                switch (currentXMLElement) {
-                    case "STN":
-                        measurement.setStationId(Integer.parseInt(nextEvent.toString()));
-                        break;
-                    case "DATE":
-                        measurementDateString = nextEvent.toString();
-                        break;
-                    case "TIME":
-                        measurement.setLocalDateTime(LocalDateTime.parse((measurementDateString + nextEvent.toString()), formatter));
-                        long t = measurement.getSecondOfDay();
-                        if (Main.Settings.SelectSeconds) {
-                            if (t % Main.Settings.SelectSecondsValue != 0) {
-                                // Drop this measurement
-                                trackElement = false;
-                            }
-                        }
-                        break;
-                    case "TEMP":
-                        measurement.setTemperature(DecimalInt.parseDecimalInt(nextEvent.toString(), 2));
-                        break;
-                    case "DEWP":
-                        measurement.setDewPoint(DecimalInt.parseDecimalInt(nextEvent.toString(),2));
-                        break;
-                    case "STP":
-                        measurement.setAirPressureStation(DecimalInt.parseDecimalInt(nextEvent.toString(),2));
-                        break;
-                    case "SLP":
-                        measurement.setAirPressureSeaLevel(DecimalInt.parseDecimalInt(nextEvent.toString(),2));
-                        break;
-                    case "VISIB":
-                        measurement.setVisibility(DecimalInt.parseDecimalInt(nextEvent.toString(),2));
-                        break;
-                    case "WDSP":
-                        measurement.setWindSpeed(DecimalInt.parseDecimalInt(nextEvent.toString(),2));
-                        break;
-                    case "PRCP":
-                        measurement.setPrecipitation(DecimalInt.parseDecimalInt(nextEvent.toString(),2));
-                        break;
-                    case "SNDP":
-                        measurement.setFallenSnow(DecimalInt.parseDecimalInt(nextEvent.toString(),2));
-                        break;
-                    case "FRSHTT":
-                        measurement.setEvents(Byte.valueOf(nextEvent.toString(), 2));
-                        break;
-                    case "CLDC":
-                        measurement.setCloudCoverage(DecimalInt.parseDecimalInt(nextEvent.toString(),2));
-                        break;
-                    case "WNDDIR":
-                        measurement.setWindDirection(DecimalInt.parseDecimalInt(nextEvent.toString(),2));
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-    }
-
-    class EmptyErrorHandler implements ErrorHandler {
-        public void fatalError( SAXParseException e ) throws SAXException {
-            throw e;
-        }
-        public void error( SAXParseException e ) throws SAXException {
-            throw e;
-        }
-        public void warning( SAXParseException e ) throws SAXException {
-            // noop
-        }
     }
 }
-
-//class Test {
-//
-//    Socket connection;
-//    MeasurementInputQueue measurementInputQueue;
-//
-//    Test() {
-//        try {
-//            measurementInputQueue = new MeasurementInputQueue();
-//            ServerSocket serverSocket = new ServerSocket(7789);
-//            connection = serverSocket.accept();
-//            connection.setSoTimeout(5000);
-//            ParallelInputSocket parallelInputSocket = new ParallelInputSocket(measurementInputQueue, connection);
-//            parallelInputSocket.run();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
 
--- src/nl/hanze/raspberryprocessor/Main.java
+++ src/nl/hanze/raspberryprocessor/Main.java
@@ -24,76 +24,65 @@ public class Main {
 class Controller {
     private MeasurementInputQueue inputQueue;
     private SemaphoreInteger connectionCount;
-    //private Hashtable<Integer, StationQueue> stationQueues;
 
     private DebugThread debugThread;
     private WeatherServer weatherServer;
-    //private StationQueueHandler stationQueueHandler;
-    //private OutputHandler outputHandler;
     private StationQueueController stationQueueController;
 
     Controller() {
-       inputQueue = new MeasurementInputQueue();
-       connectionCount = new SemaphoreInteger(0);
+        inputQueue = new MeasurementInputQueue();
+        connectionCount = new SemaphoreInteger(0);
 
-       weatherServer = new WeatherServer(inputQueue, connectionCount);
-       Thread weatherServerThread = new Thread(weatherServer);
-       weatherServerThread.start();
+        weatherServer = new WeatherServer(inputQueue, connectionCount);
+        Thread weatherServerThread = new Thread(weatherServer);
+        weatherServerThread.start();
 
-       //outputHandler = new OutputHandler();
+        //stationQueueController = new StationQueueController(inputQueue);
+        //Thread stationQueueControllerThread = new Thread(stationQueueController);
+        //stationQueueControllerThread.start();
 
-       //stationQueues = new Hashtable<Integer, StationQueue>(8000);
-       //stationQueueHandler = new StationQueueHandler(stationQueues, inputQueue, outputHandler);
-       //Thread stationQueueHandlerThread = new Thread(stationQueueHandler);
-       //stationQueueHandlerThread.start();
+        debugThread = new DebugThread(inputQueue, connectionCount);
+        Thread debugThreadThread = new Thread(debugThread);
+        debugThreadThread.start();
 
-        stationQueueController = new StationQueueController(inputQueue);
-        Thread stationQueueControllerThread = new Thread(stationQueueController);
-        stationQueueControllerThread.start();
+        if (Main.Settings.Mute) {
+            System.out.println("Started RaspberryProcessor. Output = disabled");
 
-
-       debugThread = new DebugThread(inputQueue, connectionCount);
-       Thread debugThreadThread = new Thread(debugThread);
-       debugThreadThread.start();
-
-       if (Main.Settings.Mute) {
-           System.out.println("Started RaspberryProcessor. Output = disabled");
-
-           System.setOut(new java.io.PrintStream(new java.io.OutputStream() {
-               @Override public void write(int b) {}
-           }) {
-               @Override public void flush() {}
-               @Override public void close() {}
-               @Override public void write(int b) {}
-               @Override public void write(byte[] b) {}
-               @Override public void write(byte[] buf, int off, int len) {}
-               @Override public void print(boolean b) {}
-               @Override public void print(char c) {}
-               @Override public void print(int i) {}
-               @Override public void print(long l) {}
-               @Override public void print(float f) {}
-               @Override public void print(double d) {}
-               @Override public void print(char[] s) {}
-               @Override public void print(String s) {}
-               @Override public void print(Object obj) {}
-               @Override public void println() {}
-               @Override public void println(boolean x) {}
-               @Override public void println(char x) {}
-               @Override public void println(int x) {}
-               @Override public void println(long x) {}
-               @Override public void println(float x) {}
-               @Override public void println(double x) {}
-               @Override public void println(char[] x) {}
-               @Override public void println(String x) {}
-               @Override public void println(Object x) {}
-               @Override public java.io.PrintStream printf(String format, Object... args) { return this; }
-               @Override public java.io.PrintStream printf(java.util.Locale l, String format, Object... args) { return this; }
-               @Override public java.io.PrintStream format(String format, Object... args) { return this; }
-               @Override public java.io.PrintStream format(java.util.Locale l, String format, Object... args) { return this; }
-               @Override public java.io.PrintStream append(CharSequence csq) { return this; }
-               @Override public java.io.PrintStream append(CharSequence csq, int start, int end) { return this; }
-               @Override public java.io.PrintStream append(char c) { return this; }
-           });
+            System.setOut(new java.io.PrintStream(new java.io.OutputStream() {
+                @Override public void write(int b) {}
+            }) {
+                @Override public void flush() {}
+                @Override public void close() {}
+                @Override public void write(int b) {}
+                @Override public void write(byte[] b) {}
+                @Override public void write(byte[] buf, int off, int len) {}
+                @Override public void print(boolean b) {}
+                @Override public void print(char c) {}
+                @Override public void print(int i) {}
+                @Override public void print(long l) {}
+                @Override public void print(float f) {}
+                @Override public void print(double d) {}
+                @Override public void print(char[] s) {}
+                @Override public void print(String s) {}
+                @Override public void print(Object obj) {}
+                @Override public void println() {}
+                @Override public void println(boolean x) {}
+                @Override public void println(char x) {}
+                @Override public void println(int x) {}
+                @Override public void println(long x) {}
+                @Override public void println(float x) {}
+                @Override public void println(double x) {}
+                @Override public void println(char[] x) {}
+                @Override public void println(String x) {}
+                @Override public void println(Object x) {}
+                @Override public java.io.PrintStream printf(String format, Object... args) { return this; }
+                @Override public java.io.PrintStream printf(java.util.Locale l, String format, Object... args) { return this; }
+                @Override public java.io.PrintStream format(String format, Object... args) { return this; }
+                @Override public java.io.PrintStream format(java.util.Locale l, String format, Object... args) { return this; }
+                @Override public java.io.PrintStream append(CharSequence csq) { return this; }
+                @Override public java.io.PrintStream append(CharSequence csq, int start, int end) { return this; }
+                @Override public java.io.PrintStream append(char c) { return this; }
+            });
        }
    }
 }
